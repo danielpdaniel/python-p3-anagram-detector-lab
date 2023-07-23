46eb3420e4c38b98a9a75854d641ae44ac7d4d3b
danielpdaniel <artofjquinn@gmail.com>
2023-07-23 10:09:45 -0400

    Update code with Anagram class and match method to get tests passing by comparing sorted arrays of words

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, matches):
+        word_arr = []
+        return_arr = []
+        for char in self.word:
+            word_arr.append(char)
+
+        for match in matches:
+            match_arr = []
+            for char in match:
+                match_arr.append(char)
+
+            if sorted(match_arr) == sorted(word_arr):
+                return_arr.append(match)
+
+        return return_arr
